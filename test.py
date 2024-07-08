ecd2f590a449c03306828086c82d483b3c4a77df
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-08 21:28:09 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+import math
+n = int(input("enter value of n: "))
+print(math.log2(n))
+n = int(input("enter value of n: "))
+print(math.cos(n))
+n = int(input("enter value of n: "))
+print(math.exp(n))
